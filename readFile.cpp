73904bff150a24334da940e64fc1285efed305dd
Greg McNutt <gcmcnutt@yahoo.com>
2023-05-23 22:16:56 -0700

    checkpoint debugging works

--- readFile.cpp
+++ readFile.cpp
@@ -4,36 +4,37 @@
 using namespace std;
 using namespace cv;
  
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
   // Create a VideoCapture object and open the input file
   // If the input is the web camera, pass 0 instead of the video file name
   VideoCapture cap(argv[1]); 
     
   // Check if camera opened successfully
-  if(!cap.isOpened()){
+  if (!cap.isOpened()) {
     cout << "Error opening video stream or file" << endl;
     return -1;
   }
    
   int fps = (int) cap.get(CAP_PROP_FPS);
   printf("* FPS: %d\n", fps);
-  
-  while(1){
+
+  while (1) {
  
     Mat frame;
     // Capture frame-by-frame
     cap >> frame;
   
     // If the frame is empty, break immediately
-    if (frame.empty())
+    if (frame.empty()) {
       break;
+    }
  
     // Display the resulting frame
-    imshow( "Frame", frame );
+    imshow(argv[1], frame);
  
-    // Press  ESC on keyboard to exit
-    char c=(char)waitKey(10);
-    if(c==27)
+    // Press ESC on keyboard to exit
+    char c = (char) waitKey(1000/fps);
+    if (c==27)
       break;
   }
   
